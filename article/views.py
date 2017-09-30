10cf9c02d177e8a20e459e4a9ac0743f9bb54190
KingArthurXu <Qingyu.xu@gmail.com>
2017-09-30 22:12:16 +0800

    Update views.py

--- article/views.py
+++ article/views.py
@@ -21,3 +21,20 @@ def test(request):
     return render(request, 'test.html', {'current_time': datetime.now()})
 
 
+def search_tag(request, tag) :
+    try:
+        post_list = Article.objects.filter(category = tag) #contains
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'tag.html', {'post_list' : post_list})
+
+def about_me(request) :
+    return render(request, 'aboutme.html')
+
+def archives(request) :
+    try:
+        post_list = Article.objects.all()
+    except Article.DoesNotExist :
+        raise Http404
+    return render(request, 'archives.html', {'post_list' : post_list, 
+                                            'error' : False})
