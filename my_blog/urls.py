98d345cbbb01a8818a54f918eba4777392c9c06e
LOU4213416603 <LOU4213416603@shiyanlou-no.com>
2017-09-30 21:14:30 +0800

    second

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -18,6 +18,6 @@ from article import views
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.home),
-    url(r'^(?P<my_args>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<id>\d+)/$', views.detail, name='detail'),
     url(r'^test/$', views.test),
 ]
